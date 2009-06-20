812fdcdc332f55e96f3fa7183d468b8cd042de2e
Alex Morega <alex@grep.ro>
2009-06-20 20:32:19 +0300

    note coordinates are stringified ints

--- src/grep/notespace/demo_db.py
+++ src/grep/notespace/demo_db.py
@@ -14,7 +14,7 @@ class DemoDb(object):
 
 def demo_data(db):
     db.create_note(0, {'desc': 'ROOT'}, [1, 4])
-    db.create_note(1, {'desc': 'note 1', 'x':100, 'y':100, 'w':500, 'h':300}, [2, 3])
-    db.create_note(2, {'desc': 'note 2', 'x':10, 'y':80})
-    db.create_note(3, {'desc': 'note 3', 'x':240, 'y':100})
-    db.create_note(4, {'desc': 'note 4', 'x':150, 'y':450})
+    db.create_note(1, {'desc': 'note 1', 'x':'100', 'y':'100', 'w':'500', 'h':'300'}, [2, 3])
+    db.create_note(2, {'desc': 'note 2', 'x':'10', 'y':'80'})
+    db.create_note(3, {'desc': 'note 3', 'x':'240', 'y':'100'})
+    db.create_note(4, {'desc': 'note 4', 'x':'150', 'y':'450'})
