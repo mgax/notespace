47808bb0377d6351d413d2ef3b4842553bcaa172
Alex Morega <alex@grep.ro>
2009-07-21 23:30:52 +0300

    add new properties to notes

--- grep/notespace/web_media/notes.js
+++ grep/notespace/web_media/notes.js
@@ -189,17 +189,46 @@ function setup_props(note) {
     }
     note.show_props = function() {
         var dl = $('<dl class="props">');
-        $.each(note.props, function(key) {
-            var dd = $('<dd>').text(note.props[key]).editable_field(function(value) {
-                if(typeof(note.props[key] == "number"))
-                    // coerce `value` to int
-                    value = value - 0;
-                note.props[key] = value;
-                note.post_props();
-            });
-            dl.append($('<dt>').text(key), dd);
-        });
         note.jq.append(dl);
+        dl.append('<div class="new_prop_dialog">'
+          + '  <label>name <input name="name" /></label><br />'
+          + '  <label>value <input name="value" /></label>'
+          + '</div>');
+        dl.append($('<div class="buttons">').append(
+            $('<a>[x]</a>').click(function() { dl.remove(); }),
+            $('<a>[+]</a>').click(function() { dialog.dialog('open'); })
+        ));
+        $.each(note.props, function(key) { add_prop_to_list(key, note.props[key]); });
+
+        var dialog = $('div.new_prop_dialog', note.jq).dialog({
+            autoOpen: false,
+            modal: true,
+            buttons: {
+                create: function() {
+                    var name = $('input[name=name]', dialog).val();
+                    var value = $('input[name=value]', dialog).val()
+                    set_prop(name, value, function() { add_prop_to_list(name, value); });
+                    $(this).dialog('close');
+                },
+                cancel: function() { $(this).dialog('close'); }
+            }
+        });
+
+        function add_prop_to_list(name, value) {
+            var dd = $('<dd>').text(value).editable_field(function(new_value) {
+                set_prop(name, new_value, function(){ dd.text(new_value); });
+            });
+            dl.append($('<dt>').text(name), dd);
+        }
+
+        function set_prop(name, new_value, callback) {
+            if(typeof(note.props[name]) == "number") {
+                // coerce `value` to int
+                new_value = new_value - 0;
+            }
+            note.props[name] = new_value;
+            note.post_props(callback);
+        }
     }
     note.add_button('[e]', note.show_props);
 
