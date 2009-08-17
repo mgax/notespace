bd94a0e98a2f73c014b58088b163594252353280
Alex Morega <alex@grep.ro>
2009-08-18 00:21:47 +0300

    mostly-functional lasso selection

--- cork/web_media/shell.js
+++ cork/web_media/shell.js
@@ -48,23 +48,54 @@ cork_ui.note_has_been_clicked = function(note) {
 }
 
 cork_ui.start_lasso = function(evt) {
-    if(evt.type != 'mousedown' || evt.which != 1)
-        return;
+    if(evt.type != 'mousedown' || evt.which != 1) {
+        return; // not a left-click event
+    }
 
-    var ix = evt.pageX, iy = evt.pageY;
+    if(! document.hasFocus()) {
+        // this click event will reach the document and focus it; we will
+        // handle subsequent clicks as lasso selections
+        return;
+    }
+    else {
+        evt.preventDefault();
+    }
+
+    var ix = evt.pageX, iy = evt.pageY; /* initial X, Y */
     var lasso_box = $('<div class="lasso">').appendTo(root_elem);
     lasso_box.css({top: iy, left: ix});
-    evt.preventDefault(); // FIXME: this prevents the document from receiving focus
 
     var move_handler = function(evt) {
-        var cx = evt.pageX, cy = evt.pageY;
-        var tx, xy, bx, by;
-        if(cx > ix) { tx = ix; bx = cx } else { tx = cx; bx = ix }
+        var cx = evt.pageX, cy = evt.pageY; /* current X, Y */
+        var lx, rx, ty, by; /* left,right X; top,bottom Y */
+
+        // make sure width and height will not come out negative
+        if(cx > ix) { rx = cx; lx = ix } else { rx = ix; lx = cx }
         if(cy > iy) { ty = iy; by = cy } else { ty = cy; by = iy }
-        lasso_box.css({top: ty, left: tx, width: bx - tx, height: by - ty});
-        /*$('.note').each(function() {
-            console.log(this);
-        });*/
+        lasso_box.css({top: ty, left: lx, width: rx - lx, height: by - ty});
+
+        // look for notes within the lasso
+        $('.note').each(function() {
+            var note_jq = $(this), offset = note_jq.offset();
+            var selection_index = current_selection.index(this);
+            var selected = (selection_index > -1);
+
+            var nl = offset.left, nr = note_jq.width() + nl; /* note X left, right */
+            var nt = offset.top, nb = note_jq.height() + nt; /* note Y top, bottom */
+
+            if(nl > lx && nr < rx && nt > ty && nb < by) {
+                if(! selected) {
+                    note_jq.addClass('selected');
+                    current_selection.push(this);
+                }
+            }
+            else {
+                if(selected) {
+                    note_jq.removeClass('selected');
+                    current_selection.splice(selection_index, 1);
+                }
+            }
+        });
     }
 
     root_elem.bind('mousemove', move_handler);
