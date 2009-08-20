2fd5e498a47474cee8935d012b898aef6f127db8
Alex Morega <alex@grep.ro>
2009-08-20 22:39:08 +0300

    note_model logging to screen

--- cork/web_media/note_model.js
+++ cork/web_media/note_model.js
@@ -1,6 +1,6 @@
 (function(){
 
-cork_ui.verbose_model = false;
+cork_ui.verbose_model = true;
 
 cork_ui._new_note_model = function(parent_note_model, callback) {
     var props = {desc: 'kid'};
@@ -11,7 +11,7 @@ cork_ui._new_note_model = function(parent_note_model, callback) {
     $.post('notes', form_data, function(note_id) {
         var child_model = make_note_model(note_id, props, null, []);
         if(cork_ui.verbose_model)
-            console.log('new note model', child_model.get_id());
+            cork_ui.report_info('new note model '+child_model.get_id());
         var note_children = parent_note_model.add_child(child_model, function() {
             if(callback) callback(child_model);
         });
@@ -28,7 +28,7 @@ function make_note_model(id, props, html, children) {
     return {
         set_prop: function(key, value, callback) {
             if(cork_ui.verbose_model)
-                console.log('note', id, 'setting property', key, 'to', value);
+                cork_ui.report_info('note '+id+' setting property '+key+' to '+value);
             props[key] = value;
             $.post('notes/' + id, {props: $.toJSON(props)},
                 function(data) { if(callback) callback(); });
@@ -51,7 +51,7 @@ function make_note_model(id, props, html, children) {
             // TODO: make sure we remove the child from where it came from
             var child_id = child_model.get_id();
             if(cork_ui.verbose_model)
-                console.log('note', id, 'appending child', child_id);
+                cork_ui.report_info('note '+id+' appending child '+child_id);
             children.push(child_id);
             $.post('/notes/' + child_id + '/parent', {parent_id: id},
                 function(data) { if(callback) callback(); }, 'json');
@@ -61,7 +61,7 @@ function make_note_model(id, props, html, children) {
         },
         delete: function(callback) {
             if(cork_ui.verbose_model)
-                console.log('deleting note', id);
+                cork_ui.report_info('deleting note '+id);
             $.ajax({type: 'DELETE', url: 'notes/' + id, dataType: "json",
                 success: function(data) { if(callback) callback(); }});
         }
