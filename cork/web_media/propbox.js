1d7458c448ccfeaa4a53a17bca85a3f6cf3c6d9c
Alex Morega <alex@grep.ro>
2009-08-21 19:29:02 +0300

    refined props box markup and styles

--- cork/web_media/propbox.js
+++ cork/web_media/propbox.js
@@ -1,17 +1,22 @@
 cork_ui.setup_props_box = function(root_note_jq) {
-var jq = $('<dl class="props">');
-root_note_jq.append(jq);
-jq.append('<div class="new_prop_dialog">'
+
+var box_jq = $('<div class="props_box">').appendTo(root_note_jq);
+function event_blocker(evt) { evt.stopPropagation(); }
+box_jq.click(event_blocker).mousedown(event_blocker); // to block handlers in "shell"
+
+box_jq.append($('<div class="buttons">').append(
+    $('<a>[+]</a>').click(function() { dialog_jq.dialog('open'); })
+));
+
+var heading_jq = $('<h4>').appendTo(box_jq);
+var props_jq = $('<dl>').appendTo(box_jq);
+
+props_jq.append('<div class="new_prop_dialog">'
   + '  <label>name <input name="name" /></label><br />'
   + '  <label>value <input name="value" /></label>'
   + '</div>');
-jq.append($('<div class="buttons">').append(
-    $('<a>[+]</a>').click(function() { dialog_jq.dialog('open'); })
-));
-function event_blocker(evt) { evt.stopPropagation(); }
-jq.click(event_blocker).mousedown(event_blocker); // to block handlers in "shell"
 
-var dialog_jq = $('div.new_prop_dialog', jq).dialog({
+var dialog_jq = $('div.new_prop_dialog', box_jq).dialog({
     autoOpen: false,
     modal: true,
     buttons: {
@@ -38,18 +43,26 @@ function add_prop_to_list(name, value) {
     var dd = $('<dd>').text(value).editable_field(function(new_value) {
         set_prop(name, new_value, function(){ dd.text(new_value); });
     });
-    jq.append($('<dt>').text(name), dd);
+    props_jq.append($('<dt>').text(name), dd);
 }
 
-var current_selected_notes = [];
+var current_selected_notes;
 function update_note_selection(selected_notes) {
-    jq.empty();
+    props_jq.empty();
     current_selected_notes = selected_notes;
-    if(selected_notes.length == 0)
+    if(selected_notes.length == 0) {
+        heading_jq.text('nothing selected');
         return;
-    var note_props = selected_notes[0].model.get_all_props();
-    $.each(note_props, function(key) { add_prop_to_list(key, note_props[key]); });
+    }
+    if(selected_notes.length > 1) {
+        heading_jq.text('multiple selection');
+        return;
+    }
+    var note_model = selected_notes[0].model;
+    heading_jq.text('editing ' + note_model.get_id());
+    $.each(note_model.get_all_props(), add_prop_to_list);
 }
+update_note_selection([]);
 
 return {
     update_note_selection: update_note_selection
