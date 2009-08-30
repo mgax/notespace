369bc04621b74726c76876efea77f0bd03e8d30d
Alex Morega <alex@grep.ro>
2009-08-30 11:47:53 +0300

    lazy wsgi app creation, for werkzeug reloader

--- cork/cmd.py
+++ cork/cmd.py
@@ -5,6 +5,9 @@ def bootstrap():
     from document import open_document, demo_data
     from optparse import OptionParser
 
+    global doc
+    doc = None
+
     parser = OptionParser()
     parser.add_option('-q', '--quiet', action='store_true', dest='quiet')
     options, args = parser.parse_args()
@@ -18,14 +21,13 @@ def bootstrap():
     for plugin_name in filter(None, (name.strip() for name in raw_names)):
         __import__(plugin_name)
 
-    doc = open_document(dbfile)
-
     if len(args) > 1:
         cmd = args[1]
     else:
         cmd = 'serve'
 
     if cmd == 'init':
+        doc = open_document(dbfile)
         demo_data(doc)
 
     elif cmd == 'serve':
@@ -41,24 +43,38 @@ def bootstrap():
         else:
             handler = None
 
-        app = CorkApp(doc)
         host = parser.get('testserver', 'host')
         port = parser.getint('testserver', 'port')
 
-        run_simple(host, port, app, use_reloader=True, request_handler=handler)
+        def lazy_app(environ, start_response):
+            """
+            WSGI application that creates the real app in a lazy fashion,
+            useful to prevent opening two DB connections when running
+            with the Werkzeug reloader.
+            """
+            global doc, cork_app
+            if doc is None:
+                doc = open_document(dbfile)
+                cork_app = CorkApp(doc)
+            return cork_app(environ, start_response)
+
+        run_simple(host, port, lazy_app, use_reloader=True, request_handler=handler)
         print # a blank line
 
     elif cmd == 'launchd':
         from wsginetd import serve
         from server import CorkApp
         sys.stderr = open('var/launchd.log', 'a')
+        doc = open_document(dbfile)
         serve(CorkApp(doc))
 
     elif cmd == 'interact':
         import code
+        doc = open_document(dbfile)
         code.interact(local={'doc': doc, '__name__': '__console__', '__doc__': None})
 
     else:
         print>>sys.stderr, "Unknown command \"%s\"" % cmd
 
-    doc.close()
+    if doc is not None:
+        doc.close()
